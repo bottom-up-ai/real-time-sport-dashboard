0b5906adaa22d7c3a9fe13b4d338656939861f0c
Louis <louis_daniel@outlook.fr>
2025-10-11 12:45:49 +0200

    refactor: break command in multi lines

--- database/scripts/generate-types-cli.ts
+++ database/scripts/generate-types-cli.ts
@@ -5,6 +5,12 @@ import { envs } from '@/envs';
  * 1. Include only tables from the app schema
  * 2. Exclude knex migrations tables
  */
-const command = `pnpm kysely-codegen --include-pattern="${envs.database.schema}.*" --exclude-pattern="${envs.database.schema}.knex_*" --dialect=postgres --out-file ./database.types.d.ts`;
+const command = [
+	'pnpm kysely-codegen',
+	`--include-pattern="${envs.database.schema}.*"`,
+	`--exclude-pattern="${envs.database.schema}.knex_*"`,
+	`--dialect=postgres`,
+	`--out-file ./database.types.d.ts`
+].join(' ');
 
 execSync(command, { stdio: 'inherit' });
